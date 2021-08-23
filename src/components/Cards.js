7c008cbc42aa7b3b39cd6b604aeedba25a2860d7
Elin Amren <elin.amren@gmail.com>
2021-08-23 13:50:51 +0200

    Star popup added same as info.

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,11 +1,14 @@
 import cardsMorning from "./cards-morning.js";
 
 const Cards = () => {
+  function handleClick() {
+    console.log("STAR!");
+  }
   return (
     <div className="cards-container">
       {cardsMorning.map((card) => {
         return (
-          <div key={card.id} className="cards">
+          <div key={card.id} className="cards" onClick={handleClick}>
             <img src={card.image} alt={card.title} width="150" height="150" />
             <h3>{card.title}</h3>
           </div>
